b361485eb41f14d9b3f0a07c29ccbf35b482e5c3
Baechul Kim <baechul_kim@intuit.com>
2013-06-18 12:06:43 -0700

    updated author info

--- cookbooks/baechul-myface/metadata.rb
+++ cookbooks/baechul-myface/metadata.rb
@@ -1,6 +1,6 @@
 name             'baechul-myface'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Baechul Kim'
+maintainer_email 'baechul_kim@intuit.com'
 license          'All rights reserved'
 description      'Installs/Configures baechul-myface'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
